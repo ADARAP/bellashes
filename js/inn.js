76bd8d8b8dc6624059f4a68be39c78875d97a15c
Alejandro Guiberra <alejandro.guiberra@gmail.com>
2015-08-10 16:23:46 -0500

    instagram
    js json

---
+++ js/inn.js
@@ -0,0 +1,15 @@
+$(function(){
+  var accessToken = '937551025.bba8e8a.12e6a04466fb4abca68e833bec29fc91';
+  $.getJSON('https://api.instagram.com/v1/users/self/media/recent/?access_token='+accessToken+'&callback=?',function (insta) {
+    $.each(insta.data,function (photos,src) {
+      if ( photos === 20 ) { return false; }
+      $('<a href="'+src.link+'" class="post">'+
+        '<div class="image" style="background-image:url('+src.images.standard_resolution.url+');"></div>'+
+        '<ul>'+
+        '<li><i class="fa fa-camera"></i> '+src.filter+'</li>'+
+        '<li><i class="fa fa-heart"></i> '+src.likes.count+'</li>'+
+        '<li><i class="fa fa-comment"></i> '+src.comments.count+'</li>'+
+        '</ul></a>').appendTo('#content');
+    });
+  });
+});
